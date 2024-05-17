bb4dc03f01713156f388ae340252113311c2e1dc
srinivas gonepudi <srigonepudi@gmail.com>
2024-05-17 15:21:33 +0530

    role

--- dev/backend.tf
+++ dev/backend.tf
@@ -1,7 +1,5 @@
-backend "s3" {
-    bucket         = "roboshop-terraform-state-bucket"
-    key            = "terraforminfra/catalogue-dev.state"
-    region         = "us-east-1"
-    dynamodb_table = "api-lock-dev"
+bucket         = "roboshop-terraform-state-bucket"
+key            = "terraforminfra/catalogue-dev.state"
+region         = "us-east-1"
+dynamodb_table = "api-lock-dev"
 
-}
